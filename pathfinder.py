4bccad746d5709b0fd0c1cf01ede5e9479f155fd
Shawna Cooper <scooper1920@gmail.com>
2022-04-27 16:11:19 -0400

    (finisheed)

--- pathfinder.py
+++ pathfinder.py
@@ -1,58 +1,103 @@
-from PIL import ImageColor
-import numpy as np
-
-
 from PIL import Image
 from PIL import ImageColor
 import numpy as np
+import random
 
 
-def get_data(file):
-    elevation_data = []
+class TopoMap:
+    # file_name is name of text file with elevation data
+    def __init__(self, name_txt):
+        self.name_txt = name_txt
+        self.name_png = self.name_txt[:-3] + 'png'
+        self.set_data()
 
-    with open(file, 'r') as f:
-        lines = f.readlines()
+    def set_data(self):
+        self.data = []
 
-        for i in range(len(lines)):
-            lines[i] = lines[i].split()
-            elevation_data.append([])
+        with open(self.name_txt, 'r') as f:
+            lines = f.readlines()
 
-            for j in range(len(lines[i])):
-                elevation_data[i].append(int(lines[i][j]))
-        return elevation_data
+            for i in range(len(lines)):
+                lines[i] = lines[i].split()
+                self.data.append([])
 
+                for j in range(len(lines[i])):
+                    self.data[i].append(int(lines[i][j]))
+
+        self.min_data = np.amin(self.data)
+        self.max_data = np.amax(self.data)
+        self.size_data = (len(self.data), len(self.data[0]))
+
+    def txt_to_png(self):
+        self.image = Image.new('RGBA', self.size_data, 'white')
+        self.image.save(self.name_png)
+
+        for row in range(len(self.data)):
+            for col in range(len(self.data[row])):
+                current_data_element = self.data[row][col]
+                grayscale = (current_data_element - self.min_data) // ((self.max_data - self.min_data)//256 + 1)
+                self.image.putpixel((col, row), (grayscale, grayscale, grayscale, 255))
+        self.image.save(self.name_png)
+
+    def find_greedy_path(self, starting_row):
+        row = starting_row
+        col = 0
+        self.path = []
+        current = self.data[row][col]
+        self.path.append((col, row))
+
+        while col < len(self.data[row])-1:
+            right_up = self.data[row-1][col+1]
+            right_down = self.data[row+1][col+1]
+            right = self.data[row][col+1]
+
+            diffs = [abs(current-right), abs(current-right_down), abs(current-right_up)]
+            min_diff = min(diffs)
+
+            if min_diff == diffs[0]:
+                current = right
+            elif diffs[1] == diffs[2] == min_diff:
+                current = diffs[random.randint(1,2)]
+                if current == right_down:
+                    row += 1
+                else:
+                    row -= 1
+            elif min_diff == diffs[1]:
+                current = right_down
+                row += 1
+            elif min_diff == diffs[2]:
+                current = right_up
+                row -= 1
+
+            col += 1
+            self.path.append((col, row))
+
+    def chart_greedy_path(self):
+        # self.test_answer = [(0,6), (1,5), (2,4), (3,3), (4,2), (5,3), (6,2), (7,2), (8,3), (9,4), (10,5), (11,5), (12,5), (13,6)]
+        for i in range(len(self.path)):
+            self.image.putpixel(self.path[i], ImageColor.getcolor('red', 'RGBA'))
+        self.image.save(self.name_png)
+
+
+elevation_small = TopoMap('elevation_small.txt')
+elevation_small.txt_to_png()
+'''
+list_of_paths = []
+for i in range(len(elevation_small.data)):
+    list_of_paths.append(elevation_small.find_greedy_path(i))
+for i in range(len(list_of_paths)):
+    elevation_small.chart_greedy_path(i)
+'''
+elevation_small.find_greedy_path(300)
+elevation_small.chart_greedy_path()
 
 '''
-class Pixel:
-    def __init__(self, pixels):
-        self.data = data
-    def get_rgba(self, pixels):
-        for i in range(len(pixels)):
-    def next_step(self, pixels):
-        right_up = 
-        right = 
-        right_down = 
+elevation_test = TopoMap('elevation_test.txt')
+elevation_test.txt_to_png()
+elevation_test.find_greedy_path(6)
+elevation_test.chart_greedy_path()
 '''
 
-
-def modify_image(file, new_image):
-    elevation_data = get_data(file)
-    size = (len(elevation_data), len(elevation_data))
-    image = Image.new('RGBA', size, 'white')
-    image.save(new_image)
-
-    max_elevation = np.amax(elevation_data)
-    min_elevation = np.amin(elevation_data)
-
-    for x in range(len(elevation_data)):
-        for y in range(len(elevation_data[x])):
-            current_elevation = elevation_data[x][y]
-            A = (current_elevation - min_elevation) // ((max_elevation - min_elevation)//256 + 1)
-            image.putpixel((y, x), (A, A, A, 255))
-    image.save(new_image)
-
-
-modify_image('elevation_small.txt', 'elevation_small.png')
 
 '''
 if __name__ == "__main__":
